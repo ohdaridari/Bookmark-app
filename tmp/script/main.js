44b91c04dfb12446857ec2c4ace77c23ed32784b
Daria T <daria230t@gmail.com>
2019-02-27 17:02:20 +0000

    some improvements

--- tmp/script/main.js
+++ tmp/script/main.js
@@ -1,8 +1,12 @@
+//Makes new bookmark form pop up on click
+
 let addItem = document.getElementById("addItem");
 
 addItem.addEventListener("click", function (){
     alert("It works!");
 });
+
+//Sets the color inside the color picker
 
 const color_picker = document.getElementById("colorPicker");
 const color_picker_wrapper = document.getElementById("colorPickerWrapper");
