a0c72d37dd85f668ed7acaf96db0e25195bd4776
Daria T <daria230t@gmail.com>
2019-05-31 17:21:20 +0200

    ostatnia poprawna wersja

--- tmp/script/main.js
+++ tmp/script/main.js
@@ -1,21 +1,30 @@
-let formAddItem = document.getElementById("formAddItem");
-
+let bookmarkName = document.getElementById("bookmarkName");
+let bookmarkUrl = document.getElementById("bookmarkUrl");
+let addToColection = document.getElementById("addToCollection");
+let addNewBookmark = document.getElementById("addNewBookmark");
+let bookmarksSection = document.getElementById("bookmarksSection");
 
 //Makes new bookmark form pop up on click
 let addItem = document.getElementById("addItem");
 
 addItem.addEventListener("click", function (){
     $(formAddItem).toggleClass("active");
-    console.log("dupa")
+    bookmarkName.focus()
 });
 
 //Sets the color inside the color picker
 
+let formAddItem = document.getElementById("formAddItem");
 const colorPicker = document.getElementById("colorPicker");
-const colorPickerWrapper = document.getElementById("colorPickerWrapper"); colorPicker.onchange = function() { colorPicker_wrapper.style.backgroundColor = colorPicker.value;
+const colorPickerWrapper = document.getElementById("colorPickerWrapper");
+colorPicker.onchange = function() { colorPickerWrapper.style.backgroundColor = colorPicker.value;
 }; colorPickerWrapper.style.backgroundColor = colorPicker.value;
 
+//Creating new bookmarks
+
+addNewBookmark.addEventListener("click", function () {
+    alert("dupa!")
+
+});
 
 
-let bookmarkName = document.getElementById("bookmarkName");
-let bookmarkUrl = document.getElementById("bookmarkUrl");
